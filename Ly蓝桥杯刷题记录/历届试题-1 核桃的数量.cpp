78e5e0ecd93ee176cce3516d11e0f97178b56d4b
whatiwhere <1545989712@qq.com>
2018-03-11 23:52:24 +0800

    3m11

---
+++ Ly蓝桥杯刷题记录/历届试题-1 核桃的数量.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cin>>a>>b>>c;
+	int re;
+	for(int i = 1; i<=a*b*c;i++){
+		if((i % a==0)&&(i % b==0)&&(i%c==0))
+     {
+			re=i;
+			break;
+     }
+	}
+	cout<<re;
+
+	return 0;
+}
---
+++ 陕科大训练/A.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int main (void)
+{
+	 int m,n,hp;
+	 cin >> m>>n>>hp;
+
+return 0;
+
+}
